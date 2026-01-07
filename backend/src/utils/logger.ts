d8685b4313d10ed11604bc4406190349f6d25f22
mahdimonir <mahdimoniruzzaman@gmail.com>
2026-01-07 17:28:16 +0600

    resolve pino logger error

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,16 +1,19 @@
-import pino from 'pino';
+import pino from "pino";
+
+const isDev = process.env.NODE_ENV === "development";
 
 const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
-  base: {
-    pid: false,
-  },
-  timestamp: pino.stdTimeFunctions.isoTime,
+  level: process.env.LOG_LEVEL || "info",
+  transport: isDev
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "HH:MM:ss Z",
+          ignore: "pid,hostname",
+        },
+      }
+    : undefined,
 });
 
 export default logger;
